3e63c83d12228915aa3d1d4a90c83186967f0c46
Rustam <tatarin1605@gmail.com>
2024-02-18 20:45:57 +0300

    Initial commit

---
+++ assets/script/fixtest.js
@@ -0,0 +1,184 @@
+const question2 = document.getElementById('secondTest-question');
+const choices2 = Array.from(document.getElementsByClassName('secondTest-choice'));
+const inputs2 = Array.from(document.getElementsByClassName('secondTestForm'))
+const form_check2 = Array.from(document.getElementsByClassName('secondTestFormCheck'))
+const next2 = document.getElementById('secondTest-next-test-btn');
+const progress_counter2 = document.getElementById('secondTest-progress-counter')
+const progressBarFull2 = document.getElementById('secondTest-progressBarFull')
+let currentQuestion2 = {};
+let acceptingAnswers2 = false;
+let questionCounter2 = 0;
+let score2 = 0;
+let availableQuestions2 = [];
+let answers2 = [];
+let questions2 = [
+    {
+        question: '1.Как правило, Ваши расходы по итогам месяца',
+        choice1: 'превышают запланированную сумму',
+        choice2: 'не превышают запланированную сумму',
+        choice3: 'у меня нет плана расходов на месяц',
+        choice4: 'не знаю ',
+        answer: ['2'],
+        view: true,
+    },
+    {
+        question: 'Ваши ежемесячные доходы (заработная плата, бизнес, фриланс) превышают ежемесячные расходы:',
+        choice1: 'всегда',
+        choice2: 'чаще «да», чем «нет» ',
+        choice3: 'чаще «нет», чем «да»',
+        choice4: 'никогда ',
+        choice5: 'не знаю ',
+        answer: ['1'],
+        half: ['2'],
+        view: true,
+    },
+    {
+        question: 'Какую часть своих ежемесячных доходов Вам удается откладывать в накопления:',
+        choice1: 'не знаю',
+        choice2: 'никакую',
+        choice3: 'менее 10%',
+        choice4: 'от 10 до 20%',
+        choice5: 'более 20%',
+        answer: ['3'],
+        over: ['4', '5'],
+        view: true,
+    },
+    {
+        question: 'Какую часть сделанных за месяц накоплений Вы, как правило, вкладываете в доходные активы (валюта, вклады, ценные бумаги и т.д.):',
+        choice1: 'не знаю ',
+        choice2: 'менее 30%',
+        choice3: 'от 30 до 50%',
+        choice4: 'более 50%',
+        answer: ['4'],
+        half: ['3'],
+        view: true,
+    },
+    {
+        question: 'Если взять за 100% общую сумму принадлежащих Вам активов (недвижимость, автомобили, деньги, вклады, ценные бумаги и пр.), какая доля приходится на финансовые активы (деньги, вклады, ценные бумаги):',
+        choice1: 'не знаю ',
+        choice2: 'менее 10% ',
+        choice3: 'от 10 до 20% ',
+        choice4: 'более 20% ',
+        choice5: 'более 50% ',
+        answer: ['4', '5'],
+        half: ['3'],
+        view: true,
+    },
+    {
+        question: 'Какая часть Ваших активов приходится на имущество, которое может приносить Вам доход (сдаваемая в аренду недвижимость, ценные бумаги, вклады и т.д.):',
+        choice1: 'не знаю',
+        choice2: 'таких активов не имею',
+        choice3: 'менее 20%',
+        choice4: 'от 20% до 50%',
+        choice5: 'более 50%',
+        answer: ['5'],
+        half: ['4'],
+        view: true,
+    },
+    {
+        question: 'Какой доход (в среднем) приносят Ваши активы:',
+        choice1: 'не знаю ',
+        choice2: 'никакого',
+        choice3: 'менее 5% годовых',
+        choice4: 'от 5 до 8% годовых ',
+        choice5: 'от 8 до 10% годовых ',
+        choice6: 'более 10% годовых ',
+        answer: ['5', '6'],
+        half: ['3', '4'],
+        view: true,
+    },
+    {
+        question: 'Как соотносятся между собой сумма Ваших активов (недвижимость, автомобили, деньги, вклады, ценные бумаги и пр.) и суммарные долги по кредитам и займам:',
+        choice1: 'не знаю ',
+        choice2: 'долги больше активов ',
+        choice3: 'примерно равны между собой ',
+        choice4: 'активы немного больше долгов ',
+        choice5: 'активы больше долгов в 2 раза и более',
+        answer: ['5'],
+        view: true,
+    },
+    {
+        question: 'Какая часть Ваших ежемесячных доходов уходит на платежи по кредитам:',
+        choice1: 'не знаю ',
+        choice2: 'менее 10% ',
+        choice3: 'от 10 до 20% ',
+        choice4: 'от 20 до 30% ',
+        choice5: 'более 30% ',
+        answer: ['2', '3'],
+        half: ['4'],
+        view: true,
+    },
+    {
+        question: 'На сколько месяцев жизни Вам хватит сделанных накоплений (включая деньги, валюту, банковские вклады, ценные бумаги) в случае потери своих источников дохода:',
+        choice1: 'менее 1 месяца ',
+        choice2: 'от 1 до 3 месяцев ',
+        choice3: 'от 3 до 6 месяцев ',
+        choice4: 'более 6 месяцев ',
+        answer: ['2'],
+        over: ['4'],
+        middle: ['3'],
+        view: true,
+    }
+]
+let MAX_QUESTIONS2 = 10;
+startGame2 = () => {
+    questionCounter2 = 0;
+    score2 = 0;
+    availableQuestions2 = [...questions2];
+    getNewQuestion2();
+};
+getNewQuestion2 = () => {
+
+    answers2 = [];
+    if (availableQuestions2.length === 0 || questionCounter2 >= MAX_QUESTIONS2) {
+        //go to the end page
+        document.getElementById('test-end').click()
+    }
+    if (questionCounter2 === MAX_QUESTIONS2-1){
+        next2.innerText = "Завершить"
+    }
+    questionCounter2++;
+    const questionIndex2 = Math.floor(Math.random() * availableQuestions2.length);
+    currentQuestion2 = availableQuestions2[questionIndex2];
+    question2.innerText = questionCounter2 + ". " + currentQuestion2.question;
+    progress_counter2.innerText = questionCounter2 + " из " + MAX_QUESTIONS2;
+    progressBarFull2.style.width = `${(questionCounter2 / MAX_QUESTIONS2) * 100}%`
+    inputs2.forEach((e) => {
+        e.checked = false;
+        if (currentQuestion2.view){
+            e.type = "radio"
+        } else {
+            e.type = "checkbox"
+        }
+    })
+    choices2.forEach((choice2) => {
+        const number2 = choice2.dataset['number'];
+        choice2.innerText = currentQuestion2['choice' + number2];
+        if (typeof currentQuestion2['choice' + number2] !== "undefined"){
+            form_check2[number2-1].classList.remove("d-none")
+        } else {
+            form_check2[number2-1].classList.add("d-none")
+        }
+    });
+    availableQuestions2.splice(questionIndex2, 1);
+    acceptingAnswers2 = true;
+};
+
+next2.addEventListener('click', () =>{
+    if (!acceptingAnswers2) return;
+    acceptingAnswers2 = false;
+    inputs2.forEach((elem) =>{
+        if (elem.checked){
+            const selectedAnswer2 = elem.dataset["number"];
+            answers2.push(selectedAnswer2);
+        }
+
+    })
+    if (JSON.stringify(answers2) === JSON.stringify(currentQuestion2.answer)){
+        score2++
+    }
+    console.log(score2)
+    getNewQuestion2();
+
+});
+startGame2();
---
+++ assets/script/script.js
@@ -0,0 +1,73 @@
+const animation1 = lottie.loadAnimation({
+    container: document.getElementById('logo-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: false, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/logo.json', // путь к JSON-файлу с анимацией
+});
+
+const animation2 = lottie.loadAnimation({
+    container: document.getElementById('chess-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/chess.json' // путь к JSON-файлу с анимацией
+});
+const animation3 = lottie.loadAnimation({
+    container: document.getElementById('magnifier-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/magnifier.json' // путь к JSON-файлу с анимацией
+});
+const animation4 = lottie.loadAnimation({
+    container: document.getElementById('directions-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/directions.json' // путь к JSON-файлу с анимацией
+});
+const animation5 = lottie.loadAnimation({
+    container: document.getElementById('calc-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/calc.json' // путь к JSON-файлу с анимацией
+});
+const animation6 = lottie.loadAnimation({
+    container: document.getElementById('stairs-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: false, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/stairs.json' // путь к JSON-файлу с анимацией
+});
+const animation7 = lottie.loadAnimation({
+    container: document.getElementById('sensor-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/sensor.json' // путь к JSON-файлу с анимацией
+});
+const animation8 = lottie.loadAnimation({
+    container: document.getElementById('matrix-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/matrix.json' // путь к JSON-файлу с анимацией
+});
+
+const animation9 = lottie.loadAnimation({
+    container: document.getElementById('about-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: false, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/scheme.json' // путь к JSON-файлу с анимацией
+});
+
+const animation10 = lottie.loadAnimation({
+    container: document.getElementById('end-sensor'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './assets/animations/sensor.json' // путь к JSON-файлу с анимацией
+});
